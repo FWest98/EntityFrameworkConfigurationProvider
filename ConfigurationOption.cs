61fe9b61a26d5ce982cfdcdc53a27b772ec5c496
Floris Westerman <floriswesterman@msn.com>
2020-04-28 14:19:23 +0200

    Set model key

--- ConfigurationOption.cs
+++ ConfigurationOption.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityFrameworkConfigurationProvider {
     public class ConfigurationOption {
+        [Key]
         public string Key { get; set; }
         public string Value { get; set; }
     }
