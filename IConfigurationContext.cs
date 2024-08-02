70ab9654ba2573c5ff709cfd0422aa47c7bf71f6
Floris Westerman <me@floriswesterman.nl>
2024-08-02 17:33:46 +0200

    Check proper connection and existence of config table without crashing

--- IConfigurationContext.cs
+++ IConfigurationContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace EntityFrameworkConfigurationProvider {
     public interface IConfigurationContext {
         public DbSet<ConfigurationOption> ConfigurationOptions { get; set; }
 
         // Some DbContext methods
+        public DatabaseFacade Database { get; }
         public EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
 
         public int SaveChanges();
